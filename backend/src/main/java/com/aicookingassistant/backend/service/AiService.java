6de67e71cd98429586d52652c0660ed5f55a9f53
Karthick B <officialkarthick23@gmail.com>
2026-02-28 11:04:23 +0530

    Service Logic

--- backend/src/main/java/com/aicookingassistant/backend/service/AiService.java
+++ backend/src/main/java/com/aicookingassistant/backend/service/AiService.java
@@ -1,5 +1,19 @@
 package com.aicookingassistant.backend.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AiService {
+
+    private final RestTemplate restTemplate;
+
+    public String generateRecipes(String prompt){
+
+        return "Tomato Rice, Biriyani, Fried Rice";
+    }
 
 }
--- backend/src/main/java/com/aicookingassistant/backend/service/RecipeService.java
+++ backend/src/main/java/com/aicookingassistant/backend/service/RecipeService.java
@@ -1,5 +1,29 @@
 package com.aicookingassistant.backend.service;
 
+import java.util.Arrays;
+
+import org.springframework.stereotype.Service;
+
+import com.aicookingassistant.backend.dto.RecipeRequestDto;
+import com.aicookingassistant.backend.dto.RecipeResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class RecipeService {
+
+    private final AiService aiService;
+
+    public RecipeResponseDto suggestRecipes(RecipeRequestDto request){
+        String prompt = "Suggest the best 5 dishes in "+ request.getCusine() 
+        + "with the following ingredients"+ String.join(",",request.getIngredients());
+
+        String aiResponse = aiService.generateRecipes(prompt);
+        RecipeResponseDto response = new RecipeResponseDto();
+        response.setRecipe(Arrays.asList(aiResponse.split(",")));
+
+        return response;
+    }   
 
 }
