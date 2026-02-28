b7ef7377e5f28357d12e6d94e0af40187e94acf0
Karthick B <officialkarthick23@gmail.com>
2026-02-28 12:12:43 +0530

    Created AI API response

--- backend/src/main/java/com/aicookingassistant/backend/service/AiService.java
+++ backend/src/main/java/com/aicookingassistant/backend/service/AiService.java
@@ -1,9 +1,13 @@
 package com.aicookingassistant.backend.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -11,9 +15,34 @@ public class AiService {
 
     private final RestTemplate restTemplate;
 
-    public String generateRecipes(String prompt){
+    @Value("${ai.openrouter.url}")
+    private String apiUrl;
 
-        return "Tomato Rice, Biriyani, Fried Rice";
+    @Value("${ai.openrouter.token}")
+    private String apiToken;
+
+    public String generateRecipes(String prompt) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(apiToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, Object> body = Map.of(
+                "model", "openai/gpt-3.5-turbo",
+                "messages", List.of(
+                        Map.of("role", "user", "content", prompt)
+                )
+        );
+
+        HttpEntity<Map<String, Object>> entity =
+                new HttpEntity<>(body, headers);
+
+        ResponseEntity<Map> response =
+                restTemplate.postForEntity(apiUrl, entity, Map.class);
+
+        Map choice = (Map)((List)response.getBody().get("choices")).get(0);
+        Map message = (Map) choice.get("message");
+
+        return message.get("content").toString();
     }
-
 }
